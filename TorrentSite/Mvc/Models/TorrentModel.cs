28611d802728d9ea41038f177989268db5179813
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-07-17 21:53:04 +0300

    Updated Torrent model

--- TorrentSite/Mvc/Models/TorrentModel.cs
+++ TorrentSite/Mvc/Models/TorrentModel.cs
@@ -7,18 +7,24 @@ namespace SitefinityWebApp.Mvc.Models
 {
     public class TorrentModel
 	{
+        [Required]
 	    public string Title { get; set; }
 
+        [Required]
         [DataType(DataType.MultilineText)]
 	    public string Description { get; set; }
 
+        [Required]
         [DataType(DataType.MultilineText)]
 	    public string AdditionalInfo { get; set; }
 
+        [Required]
 	    public string DownloadLink { get; set; }
 
+        [Required]
 	    public string Genre { get; set; }
 
+        [Required]
 	    public DateTime TorrentDateCreated { get; set; }
 
 	    public Image Image { get; set; }
